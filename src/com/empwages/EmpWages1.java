29f07d11328f44c81899f66c157f34ede590d6c3
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-02-22 22:17:13 +0530

    Checking Employee is present or not

---
+++ src/com/empwages/EmpWages1.java
@@ -0,0 +1,26 @@
+package com.empwages;
+
+public class EmpWages1 {
+	private void empstatus() {
+		// number for 0 for absent 1 for present
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		// System.out.println(empCheck);
+
+		// if condition to check employee is absent for 0
+		if (empCheck == 0) {
+			System.out.println("Employee is Absent");
+		}
+
+		// else condition to check employee is present for 1
+		else
+			System.out.println("Employee is Present");
+	}
+
+	public static void main(String[] args)
+	{
+		EmpWages1 ew = new EmpWages1();
+		//declaring constants
+		ew.empstatus();
+	}
+
+}
